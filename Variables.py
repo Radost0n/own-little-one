a9d14e49037ee76709f621a0a1351168964a387e
Radost0n <154574169+Radost0n@users.noreply.github.com>
2024-10-13 21:01:18 +1000

    Delete Variables.py

--- Variables.py
+++
@@ -1,6 +0,0 @@
-num_comp_hom = 12
-num_hour_spent = 1.5
-cour_tit = "Python"
-clas_time = (num_comp_hom / num_hour_spent)
-print(clas_time)
-print(f"Курс: {cour_tit}, всего задач: {num_comp_hom}, затрачено часов: {num_hour_spent}, среднее время выполнения: {clas_time} часа.")
